feb0d62bbf3057e27ad5e3136eb5be4c15ce4c47
Clay <clayw@udel.edu>
2023-11-02 18:07:14 -0400

    got 1b working

--- question1.py
+++ question1.py
@@ -4,7 +4,7 @@ from simon import SimonCipher
 from Crypto.Hash import Poly1305
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
-from Crypto.Hash.HMAC import HMAC
+from Crypto.Hash import HMAC
 from Crypto.Hash import SHA256
 from speck import SpeckCipher
 
@@ -51,11 +51,72 @@ def eam_decrypt_and_verify(ctxt, mac, k1, k2, iv, mac_nonce):
 
     if mac == vmac:
         print("MAC verification successful.")
+        return ptxt
     else:
         print("MAC verification failed.")
-        ptxt = None
+        return None
 
-    return ptxt
+def mte_encrypt(ptxt, k1, k2, iv):
+    ptxt_bytes = ptxt.encode()
+    #print("Plaintext: ", ptxt_bytes.decode('utf-8'))
+    iv_int = int.from_bytes(iv, byteorder='big')
+    k1_int = int.from_bytes(k1, byteorder='big')
+    hmac = HMAC.new(key=k2, msg=ptxt_bytes, digestmod=SHA256)
+    mac = hmac.digest() #in bytes
+    #print("MAC: ", mac.hex())
+
+
+    ptxt_mac = ptxt_bytes + mac
+    #print(ptxt_mac)
+
+    #print("Plaintext+MAC: ", ptxt_mac.hex())
+
+    speck_cipher = SpeckCipher(k1_int, block_size=128, key_size=128, mode='CBC', init=iv_int)
+    ctxt = b''
+    for i in range(0, len(ptxt_mac), 16):
+        block = ptxt_mac[i:i+16]
+        if len(block) < 16:
+            block = pad(block, 16)
+        block_int = int.from_bytes(block, byteorder='big')
+        ctxt_block = speck_cipher.encrypt(block_int)
+        ctxt += ctxt_block.to_bytes(16, byteorder='big')
+    ctxt_int = int.from_bytes(ctxt, byteorder='big')
+    
+    return ctxt_int
+
+def mte_decrypt_and_verify(ctxt, k1, k2, iv):
+    iv_int = int.from_bytes(iv, byteorder='big')
+    k1_int = int.from_bytes(k1, byteorder='big')
+    speck_cipher = SpeckCipher(k1_int, block_size=128, key_size=128, mode='CBC', init=iv_int)
+    ctxt_bytes = ctxt.to_bytes((ctxt.bit_length() + 7) // 8, byteorder='big')
+    ptxt_mac = b''
+    for i in range(0, len(ctxt_bytes), 16):
+        block = ctxt_bytes[i:i+16]
+        block_int = int.from_bytes(block, byteorder='big')
+        ptxt_mac_block = speck_cipher.decrypt(block_int)
+        ptxt_mac += ptxt_mac_block.to_bytes(16, byteorder='big')
+
+    try:
+        ptxt_mac = unpad(ptxt_mac, 16)
+    except ValueError as e:
+        raise ValueError("Incorrect padding or corrupted data") from e
+
+    ptxt = ptxt_mac[:-32]
+    print("Plaintext: ", ptxt)
+    mac = ptxt_mac[-32:]
+    print("MAC: ", mac)
+    
+
+    hmac = HMAC.new(k2, digestmod=SHA256)
+    hmac.update(ptxt)
+    try:
+        hmac.verify(mac)
+        print("MAC verification successful.")
+        return ptxt.decode()
+    except ValueError:
+        print("MAC verification failed.")
+        return None
+
 
 def poly(k2, ptxt):
     #k2_bytes = bytes.fromhex(hex(k2).lstrip('0x'))
@@ -74,13 +135,34 @@ k1 = get_random_bytes(16)
 k2 = get_random_bytes(32)
 plaintext = "Hello, world!"
 
-print("Original Plaintext:", plaintext)
+print("Original Plaintext: ", plaintext)
 ctxt, mac, mac_nonce = eam_encrypt(plaintext, k1, k2, iv)
 decrypted = eam_decrypt_and_verify(ctxt, mac, k1, k2, iv, mac_nonce)
-decrypted = decrypted
-print("Decrypted Text:", decrypted)
+print("Decrypted Text: ", decrypted)
 
 assert plaintext == decrypted
 
+plaintext = "This is a really long message, I'm talking really long, like longer than 128 bits kind of long, I'm talking so long that it will really test my functions block splitting capabilities kind of long."
+print("Original Plaintext: ", plaintext)
+ctxt, mac, mac_nonce = eam_encrypt(plaintext, k1, k2, iv)
+decrypted = eam_decrypt_and_verify(ctxt, mac, k1, k2, iv, mac_nonce)
+print("Decrypted Text: ", decrypted)
 
+assert plaintext == decrypted
 
+#Testing 1b
+plaintext = "Hello, world!"
+print("Original Plaintext:", plaintext)
+ctxt = mte_encrypt(plaintext, k1, k2, iv)
+decrypted = mte_decrypt_and_verify(ctxt, k1, k2, iv)
+print("Decrypted Text: ", decrypted)
+
+assert plaintext == decrypted
+
+plaintext = "This is a really long message, I'm talking really long, like longer than 128 bits kind of long, I'm talking so long that it will really test my functions block splitting capabilities kind of long."
+print("Original Plaintext:", plaintext)
+ctxt = mte_encrypt(plaintext, k1, k2, iv)
+decrypted = mte_decrypt_and_verify(ctxt, k1, k2, iv)
+print("Decrypted Text: ", decrypted)
+
+assert plaintext == decrypted
